a957b727627165644306f1e6b35424a70c6de213
LAPTOP-90O3I2F7\yulik <yulikeren3@gmail.com>
2026-02-15 21:28:43 +0200

    added recycleview to allergies and restrictions.

--- app/src/main/java/com/project/fridgemate/ui/profile/MyProfileFragment.kt
+++ app/src/main/java/com/project/fridgemate/ui/profile/MyProfileFragment.kt
@@ -7,7 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.project.fridgemate.R
-
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 class MyProfileFragment : Fragment() {
 
     override fun onCreateView(
@@ -24,5 +25,26 @@ class MyProfileFragment : Fragment() {
         view.findViewById<View>(R.id.btnBack).setOnClickListener {
             findNavController().navigateUp()
         }
+        setupAllergies(view)
+    }
+    private fun setupAllergies(view: View) {
+        val allergies = listOf(
+            AllergyItem("Peanuts"),
+            AllergyItem("Tree Nuts"),
+            AllergyItem("Dairy"),
+            AllergyItem("Eggs"),
+            AllergyItem("Soy"),
+            AllergyItem("Wheat/Gluten"),
+            AllergyItem("Fish"),
+            AllergyItem("Shellfish"),
+            AllergyItem("Sesame")
+        )
+        val adapter = AllergyAdapter(allergies)
+        view.findViewById<RecyclerView>(R.id.rvAllergies).apply {
+            this.adapter = adapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+        // val selected = adapter.getSelectedAllergies()
+        // TODO: PUT /users/me { "allergies": selected }
     }
 }
