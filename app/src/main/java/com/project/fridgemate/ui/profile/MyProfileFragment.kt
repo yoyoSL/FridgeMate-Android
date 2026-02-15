3874977d08a84dba92ae9543a6b884a27ca3237f
LAPTOP-90O3I2F7\yulik <yulikeren3@gmail.com>
2026-02-15 21:57:06 +0200

    added "add picture" from gallery option, with premission to get into the gallery

--- app/src/main/java/com/project/fridgemate/ui/profile/MyProfileFragment.kt
+++ app/src/main/java/com/project/fridgemate/ui/profile/MyProfileFragment.kt
@@ -1,16 +1,31 @@
 package com.project.fridgemate.ui.profile
-
+import android.Manifest
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.project.fridgemate.R
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
+import com.project.fridgemate.R
 class MyProfileFragment : Fragment() {
-
+    private val pickImageLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            uri?.let {
+                view?.findViewById<ShapeableImageView>(R.id.profile_pic)
+                    ?.setImageURI(it)
+            }
+        }
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                pickImageLauncher.launch("image/*")
+            }
+        }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,6 +41,9 @@ class MyProfileFragment : Fragment() {
             findNavController().navigateUp()
         }
         setupAllergies(view)
+        view.findViewById<View>(R.id.btnChangePhoto).setOnClickListener {
+            requestPermissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+        }
     }
     private fun setupAllergies(view: View) {
         val allergies = listOf(
